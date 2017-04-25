c7ec08f9a6d87456c320846b3c6b50fe77e521e5
dmonakhov <mjelnr@gmail.com>
2017-04-25 11:18:48 +0200

    Fix bug with castling.

--- public/javascripts/move.js
+++ public/javascripts/move.js
@@ -1,10 +1,9 @@
 function doMove(move) {
     var from = move[0] + move[1]
     var to = move[2] + move[3]
-    board.move(from + '-' + to)
     var move = { from: from, to: to,
         promotion: move[4] == undefined ? 'q' : move[4] }
-    console.log('Move: ' + move.from + ' - ' + move.to + ', ' + move.promotion)
     game.move(move)
+    board.position(game.fen())
     updateStatus()
 }
