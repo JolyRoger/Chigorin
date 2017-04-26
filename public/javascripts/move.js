1f263dd17bf9197c3b5e4ff63ad3def8eb5e21c5
dmonakhov <mjelnr@gmail.com>
2017-04-26 17:59:28 +0200

    Move up/down buttons have been added.

--- public/javascripts/move.js
+++ public/javascripts/move.js
@@ -7,3 +7,16 @@ function doMove(move) {
     board.position(game.fen())
     updateStatus()
 }
+
+function moveDownShow() {
+	var $spanArray = $('div#notation').find('span').not('.toDelete')
+	spanIndex = $spanArray.index($spanArray.last()) - 1
+	if (spanIndex >= 0) $spanArray[spanIndex].click()
+    continueAnalysis(clearAndAnalyse)
+}
+
+function moveUpShow() {
+	var $spanArray = $('div#notation').find('span.toDelete')
+	if ($spanArray.length > 0) $spanArray[0].click()
+    continueAnalysis(clearAndAnalyse)
+}
